a2a40d9c5fa23d8e1822788d1e7a20fb0aa36317
Alejandro Reynoso <alexj.rey23@gmail.com>
2025-10-09 20:49:37 -0700

    Finished cleaning up LED code.

--- frc1388-elevator-2025/src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ frc1388-elevator-2025/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -12,6 +12,7 @@ import frc.robot.Constants.LEDConstants;
 import com.ctre.phoenix6.configs.CANdleConfiguration;
 import com.ctre.phoenix6.configs.LEDConfigs;
 import com.ctre.phoenix6.controls.FireAnimation;
+import com.ctre.phoenix6.controls.RainbowAnimation;
 import com.ctre.phoenix6.controls.SingleFadeAnimation;
 import com.ctre.phoenix6.controls.SolidColor;
 // import com.ctre.phoenix.led.CANdle;
@@ -38,54 +39,54 @@ public class LEDSubsystem extends SubsystemBase {
   private static final int kSlotEnd = 46;
   
 
-
-
-
   /** Creates a new LEDSubsystem. */  
   public LEDSubsystem(CANdle candle) {
+
     m_candle = candle;
+
     // m_isOnRed = (DriverStation.getAlliance().get() == Alliance.Red);
-      CANdleConfiguration config = new CANdleConfiguration();
-      config.withLED(new LEDConfigs().withStripType(StripTypeValue.RGB).withBrightnessScalar(1));
-     m_candle.getConfigurator().apply(config);
-     
-     
-      // m_candle.clearAnimation(0);
-      // m_candle.clearAnimation(1);
-      // m_candle.clearAnimation(2);
 
-    // SingleFadeAnimation fades = new SingleFadeAnimation(247, 233, 0, 0, 0.2, 47, 0);
-    // m_candle.animate(fades);
+    CANdleConfiguration config = new CANdleConfiguration();
+    config.withLED(new LEDConfigs().withStripType(StripTypeValue.RGB).withBrightnessScalar(1));
+    m_candle.getConfigurator().apply(config);
+    
+    setSolidWhite();
 
-    // Yellow-Green fade in animation
-    // SingleFadeAnimation fades = new SingleFadeAnimation(8, 46).withSlot(0).withColor(new RGBWColor(247, 233, 0, 0).scaleBrightness(.5
-    // )).withFrameRate(0.2);
-    // m_candle.setControl(fades);
+  } // end LEDSubsystem() constructor
 
-    // Solid White Light at 50%
-    m_candle.setControl(new SolidColor(kSlotStart, kSlotEnd).withColor(kWhite));
-    m_candle.setControl(new SolidColor(kSlotStart, kSlotEnd).withColor(new RGBWColor(Color.kOrangeRed)));
-   
-    // Rainbow animation
+  public void setSolidWhite(){
+    m_candle.setControl(
+        new SolidColor(kSlotStart, kSlotEnd)
+          .withColor(new RGBWColor(Color.kWhite).scaleBrightness(0.5))
+          );
+  }
 
-
+  public void setFireAnimation(){
     // Fire animation
     m_candle.setControl(
         new FireAnimation(kSlotStart, kSlotEnd).withSlot(1)
             .withDirection(AnimationDirectionValue.Backward)
             .withCooling(0.4)
-            .withSparking(0.5)
-            // .withColor(new RGBWColor(Color.kOrangeRed))
-    );
+            .withSparking(0.5));
+  }
 
+  public void setRainbowAnimation(){
+    m_candle.setControl(
+        new RainbowAnimation(kSlotStart, kSlotEnd).withSlot(2)
+            .withDirection(AnimationDirectionValue.Forward)
+            .withFrameRate(200));
+  }
 
-    // Eagle Robotics Blue/Yellow animation
+  public void setStrobeAnimation(){
 
+  }
 
-
-}
-
-
+  public void setFadeAnimation(){
+    // Yellow-Green fade in animation
+    SingleFadeAnimation fades = new SingleFadeAnimation(8, 46).withSlot(0)
+        .withColor(new RGBWColor(247, 233, 0, 0).scaleBrightness(.5)).withFrameRate(0.2);
+    m_candle.setControl(fades);
+  }
 
   @Override
   public void periodic() {
